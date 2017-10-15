7e61416247bd7787724ddcf44384ce54af652046
garyyh78 <hangyu@gmail.com>
2017-10-15 20:48:41 +0800

    fix some errors

--- dsaae.py
+++ dsaae.py
@@ -1,4 +1,9 @@
-import matplotlib.pyplot as plt
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import datetime
+
 import numpy as np
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
@@ -15,17 +20,22 @@ print("total train samples = ", n_samples, "\n")
 # network meta data
 MNIST_DIM = 28
 net_metadata = {'input_size': MNIST_DIM * MNIST_DIM,
+
                 'infer_layer_1_size': 500,
                 'infer_layer_2_size': 500,
                 'latent_z_size': 100,
                 'gen_layer_1_size': 500,
                 'gen_layer_2_size': 500,
+
                 'action_function': tf.nn.relu,
-                'output_function': tf.nn.sigmoid,
+                'output_function': tf.nn.sigmoid,  # final output layer, has to be bounced
+                'eps': 1e-10,
+
                 'batch_size': 100,
                 'learning_rate': 0.0001,
-                'training_epoch': 300,
+                'training_epoch': 500,
                 'display_step': 10,
+
                 'model_path': "./models",
                 }
 
@@ -56,6 +66,8 @@ weights['infer_b2'] = tf.get_variable("infer_b2",
                                       [net_metadata['infer_layer_2_size']],
                                       tf.float32,
                                       tf.zeros_initializer())
+
+# x --> gen_L1 --> gen_L2 --> x_out --> ( z_u, z_log_s2 )
 activate = net_metadata['action_function']
 infer_L1 = activate(tf.add(tf.matmul(x,
                                      weights['infer_h1']),
@@ -69,12 +81,12 @@ weights['latent_h_u'] = tf.get_variable("latent_h_u",
                                         [net_metadata['infer_layer_2_size'],
                                          net_metadata['latent_z_size']],
                                         tf.float32,
-                                        tf.zeros_initializer())
+                                        tf.contrib.layers.xavier_initializer())
 weights['latent_h_s'] = tf.get_variable("latent_h_s",
                                         [net_metadata['infer_layer_2_size'],
                                          net_metadata['latent_z_size']],
                                         tf.float32,
-                                        tf.zeros_initializer())
+                                        tf.contrib.layers.xavier_initializer())
 weights['latent_b_u'] = tf.get_variable("latent_b_u",
                                         [net_metadata['latent_z_size']],
                                         tf.float32,
@@ -84,6 +96,7 @@ weights['latent_b_s'] = tf.get_variable("latent_b_s",
                                         tf.float32,
                                         tf.zeros_initializer())
 
+# re-parameterization trick: ( z_u, z_log_s2 ) --> z
 # Q1: do we need to add activation func here ?
 # Q2: do we want to train log_s2, or log_s or s ?
 z_u = tf.add(tf.matmul(infer_L2,
@@ -129,21 +142,26 @@ weights['gen_b_out'] = tf.get_variable("gen_b_out",
                                        tf.float32,
                                        tf.zeros_initializer())
 
+# z --> gen_L1 --> gen_L2 --> x_out
 gen_L1 = activate(tf.add(tf.matmul(z,
                                    weights['gen_h1']),
                          weights['gen_b1']))
 gen_L2 = activate(tf.add(tf.matmul(gen_L1,
                                    weights['gen_h2']),
                          weights['gen_b2']))
-x_out = activate(tf.add(tf.matmul(gen_L2,
-                                  weights['gen_h_out']),
-                        weights['gen_b_out']))
+
+output = net_metadata['output_function']
+x_out = output(tf.add(tf.matmul(gen_L2,
+                                weights['gen_h_out']),
+                      weights['gen_b_out']))
 
 # define loss
-recon_loss = -tf.reduce_sum(x * tf.log(1e-10 + x_out) +
-                            (1 - x) * tf.log(1e-10 + 1 - x_out),
+tiny = net_metadata['eps']
+recon_loss = -tf.reduce_sum(x * tf.log(tiny + x_out) +
+                            (1 - x) * tf.log(tiny + 1 - x_out),
                             1)
 
+# closed form for KL
 latent_loss = -0.5 * tf.reduce_sum(1 + z_log_s2 -
                                    tf.square(z_u) -
                                    tf.exp(z_log_s2), 1)
@@ -161,53 +179,43 @@ def run(sess, saver, net_metadata, n_samples, mnist, optimizer, J):
         avg_cost = 0.0
         total_batch = int(n_samples / bs)
         for i in range(total_batch):
-            batch_x, _label = mnist.train.next_batch(bs)
-            _opt, cost = sess.run((optimizer, J), feed_dict={x: batch_x})
+            batch_x, _ = mnist.train.next_batch(bs)
+            _, cost = sess.run((optimizer, J), feed_dict={x: batch_x})
             avg_cost += cost / n_samples * bs
 
         # Display logs per epoch step
         if epoch % net_metadata['display_step'] == 0:
             saver.save(sess, net_metadata['model_path'])
-            print("Epoch:", '%04d' % (epoch + 1),
-                  "cost=", "{:.9f}".format(avg_cost))
-
-    sess.close()
+            print("Epoch:", '%03d' % (epoch + 1),
+                  "avg_cost=", "{:.4f}".format(avg_cost), "time=", datetime.datetime.now())
 
 
 # Main body
-init = tf.global_variables_initializer()
-sess = tf.InteractiveSession()
-saver = tf.train.Saver()
+saver = tf.train.Saver(max_to_keep=1)
+model_file = tf.train.latest_checkpoint(net_metadata['model_path'])
+print("model file = %s" % model_file)
 
-mode = "restart"
+mode = "train"
+print("run as %s .. " % mode)
 
-if mode == "restart":
-    print("start init ...\n")
-    sess.run(init)
-    run(sess, saver, net_metadata, n_samples, mnist, optimizer, J)
+with tf.Session() as sess:
+    if mode == "train":
+        if model_file:
+            saver.restore(sess, model_file)
+            print("restored from %s" % model_file)
+        else:
+            init = tf.global_variables_initializer()
+            sess.run(init)
+            print("start init ...\n")
 
-if mode == "continue":
-    print("continue with saved model .. \n")
-    model_file = tf.train.latest_checkpoint(net_metadata['model_path'])
-    saver.restore(sess, model_file)
-    run(sess, saver, net_metadata, n_samples, mnist, optimizer, J)
+        run(sess, saver, net_metadata, n_samples, mnist, optimizer, J)
 
-if mode == "testing":
-    print("testing saved model .. \n")
-    model_file = tf.train.latest_checkpoint(net_metadata['model_path'])
-    saver.restore(sess, model_file)
-    batch_x, _label = mnist.train.next_batch(net_metadata['batch_size'])
-    x_recon = sess.run(x_out, feed_dict={x: batch_x})
+    elif mode == "eval":
+        saver.restore(sess, model_file)
+        print("restored from %s" % model_file)
 
-    plt.figure(figsize=(8, 12))
-    for i in range(5):
-        plt.subplot(5, 2, 2 * i + 1)
-        plt.imshow(batch_x[i].reshape(28, 28), vmin=0, vmax=1, cmap="gray")
-        plt.title("Test input")
-        plt.colorbar()
-        plt.subplot(5, 2, 2 * i + 2)
-        plt.imshow(x_recon[i].reshape(28, 28), vmin=0, vmax=1, cmap="gray")
-        plt.title("Reconstruction")
-        plt.colorbar()
-    plt.tight_layout()
-    plt.show()
+        batch_x, _label = mnist.test.next_batch(net_metadata['batch_size'])
+        cost = sess.run(J, feed_dict={x: batch_x})
+        print("test cost = %f" % cost)
+
+    sess.close()
